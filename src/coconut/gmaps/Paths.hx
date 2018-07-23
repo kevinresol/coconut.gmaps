4fd87bee8c4ea367b11983513e49d1516f99caf3
Kevin Leung <kevinresol@gmail.com>
2018-07-23 17:12:55 +0800

    Add geojson conversion

--- src/coconut/gmaps/Paths.hx
+++ src/coconut/gmaps/Paths.hx
@@ -1,6 +1,5 @@
 package coconut.gmaps;
 
-import google.maps.Polygon;
 import google.maps.LatLng;
 import tink.pure.List;
 
@@ -12,7 +11,7 @@ abstract Paths(List<List<LatLngLiteral>>) from List<List<LatLngLiteral>> to List
 	}
 	
 	@:from
-	public static function fromPolygon(polygon:Polygon):Paths {
+	public static function fromPolygon(polygon:google.maps.Polygon):Paths {
 		var arr = [];
 		polygon.getPaths().forEach(function(path, _) {
 			var verts:Array<LatLngLiteral> = [];
@@ -21,6 +20,24 @@ abstract Paths(List<List<LatLngLiteral>>) from List<List<LatLngLiteral>> to List
 		});
 		return fromArray(arr);
 	}
+	
+	#if geojson
+	@:to
+	public function toGeoJson():geojson.Polygon {
+		return new geojson.Polygon([for(path in this) [for(v in path) new geojson.util.Coordinates(v.lat, v.lng)]]);
+	}
+	
+	@:from
+	public static function fromGeoJson(polygon:geojson.Polygon):Paths {
+		var arr = [];
+		for(line in polygon.lines) {
+			var verts = [];
+			for(point in line) verts.push(new LatLngLiteral(point.latitude, point.longitude));
+			arr.push(verts);
+		}
+		return fromArray(arr);
+	}
+	#end
 	
 	public function deleteVertex(path:Int, vertex:Int):Paths {
 		var arr = [for(path in this) [for(v in path) v]];
