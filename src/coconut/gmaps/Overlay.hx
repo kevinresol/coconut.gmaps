08639b010ec6294baee1069157989d92d0631295
Kevin Leung <kevinresol@gmail.com>
2018-12-28 21:30:24 +0800

    Make Overlay a generic div container. So that it can accept arbitrary children just like InfoWindow

--- src/coconut/gmaps/Overlay.hx
+++ src/coconut/gmaps/Overlay.hx
@@ -10,68 +10,10 @@ import tink.pure.*;
 using tink.CoreApi;
 
 typedef OverlayData = {
-	@:optional var type:OverlayType;
+	var bounds:LatLngBounds;
+	@:optional var children:coconut.ui.RenderResult;
 }
 
 class Overlay extends ObjectBase<OverlayData> {
 	override function toType() return OOverlay(this);
-}
-
-enum OverlayType {
-	Image(source:String, bounds:LatLngBounds);
-	Custom(view:google.maps.OverlayView);
-}
-
-@:allow(coconut.gmaps)
-class ImageOverlayView extends google.maps.OverlayView {
-	var div:Element;
-	
-	var source:String;
-	var bounds:LatLngBounds;
-	
-	function new(source, bounds) {
-		super();
-		this.source = source;
-		this.bounds = bounds;
-	}
-	
-	override function onAdd() {
-		div = document.createDivElement();
-		div.style.borderStyle = 'none';
-		div.style.borderWidth = '0';
-		div.style.position = 'absolute';
-		
-		var img = document.createImageElement();
-		img.src = source;
-		img.style.width = '100%';
-		img.style.height = '100%';
-		img.style.position = 'absolute';
-		div.appendChild(img);
-		
-		getPanes().overlayLayer.appendChild(div);
-	}
-	
-	override function draw() {
-		var projection = getProjection();
-		var sw = projection.fromLatLngToDivPixel(bounds.getSouthWest());
-		var ne = projection.fromLatLngToDivPixel(bounds.getNorthEast());
-		div.style.left = sw.x + 'px';
-		div.style.top = ne.y + 'px';
-		div.style.width = (ne.x - sw.x) + 'px';
-		div.style.height = (sw.y - ne.y) + 'px';
-	}
-	
-	override function onRemove() {
-		div.parentNode.removeChild(div);
-		div = null;
-	}
-}
-
-class OverlayTypeTools {
-	public static function toGoogle(type:OverlayType):google.maps.OverlayView {
-		return switch type {
-			case Image(source, bounds): new ImageOverlayView(source, bounds);
-			case Custom(view): view;
-		}
-	}
 }
--- src/coconut/gmaps/ref/OverlayRef.hx
+++ src/coconut/gmaps/ref/OverlayRef.hx
@@ -1,21 +1,67 @@
 package coconut.gmaps.ref;
 
+import js.html.Element;
+import js.Browser.*;
 import google.maps.*;
 
 using coconut.gmaps.Overlay;
 
 @:forward
-abstract OverlayRef(RefBase<google.maps.OverlayView, OverlayData>) {
+abstract OverlayRef(RefBase<OverlayView, OverlayData>) {
 	public inline function new() {
 		this = new RefBase(
-			(null:google.maps.OverlayView),
+			new OverlayView(),
 			function(v) v.setMap(null)
 		);
 	}
 	
 	public inline function setup(map:google.maps.Map, data:OverlayData) {
-		if(this.ref != null) this.ref.setMap(null);
-		this.ref = data.type.toGoogle();
-		this.ref.setMap(map);
+		this.ref.bounds = data.bounds; // TODO: might need to call draw() manually here
+		this.ref.content = data.children.toElement();
+		if(this.ref.getMap() != map) this.ref.setMap(map);
+	}
+}
+
+class OverlayView extends google.maps.OverlayView {
+	
+	public var content(never, set):Element;
+	public var bounds:LatLngBounds;
+	
+	var div:Element;
+	
+	public function new(?bounds) {
+		super();
+		this.bounds = bounds;
+		div = document.createDivElement();
+		div.style.borderStyle = 'none';
+		div.style.borderWidth = '0';
+		div.style.position = 'absolute';
+	}
+	
+	override function onAdd() {
+		getPanes().overlayLayer.appendChild(div);
+	}
+	
+	override function draw() {
+		if(bounds != null) {
+			var projection = getProjection();
+			var sw = projection.fromLatLngToDivPixel(bounds.getSouthWest());
+			var ne = projection.fromLatLngToDivPixel(bounds.getNorthEast());
+			div.style.left = sw.x + 'px';
+			div.style.top = ne.y + 'px';
+			div.style.width = (ne.x - sw.x) + 'px';
+			div.style.height = (sw.y - ne.y) + 'px';
+		}
+	}
+	
+	override function onRemove() {
+		div.parentNode.removeChild(div);
+	}
+	
+	function set_content(e:Element):Element {
+		while(div.firstChild != null) div.removeChild(div.firstChild);
+		console.log(e);
+		if(e != null) div.appendChild(e);
+		return e;
 	}
 }
